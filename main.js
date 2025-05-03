f1d25fe4364059c2ac123c4bd83544243291912d
a <102840087+kingdudely@users.noreply.github.com>
2025-05-03 10:41:23 -0400

    Blud got cut in half

--- main.js
+++ main.js
@@ -1,240 +1,138 @@
-String.prototype.lastIndexNotOf = function(searchString) {
-    for (var i = this.length - 1; i >= 0; i--) {
-        for (var x = 0; x < searchString.length; x++) {
-            if (searchString.substr(x, 1) != this.substr(i - 1, 1) && x + 1 == searchString.length) {
-                return i;
+{
+    const apiUrl = "https://discord.com/api/v9";
+
+    var Discord = {
+        get: function (name) {
+            let webpackRequire; // get all webpack modules
+
+            webpackChunkdiscord_app.push([
+                [Date.now()],
+                {},
+                result => webpackRequire = result,
+            ]);
+
+            for (const { exports, id } of Object.values(webpackRequire.c)) {
+                if (typeof(exports?.[name]) === "function") {
+                    return exports[name];
+                }
+                else if (id === name) {
+                    return exports;
+                }
+                /*
+                try {
+                    return exports[name]();
+                }
+                catch {
+                    if (id === name) {
+                        return exports;
+                    }
+                } 
+                */
             }
-        }
-    }
-    return -1;
-}
+        },
 
-String.prototype.splitNth = function(nth) {
-    let result = [];
+        User: class {
+            #fetchApi(api, method, body) {
+                return fetch(apiUrl + api, {
+                    "headers": {
+                        "authorization": this.token,
+                        "content-type": "application/json",
+                    },
+                    body: JSON.stringify(body),
+                    method
+                })
+            };
 
-    for (let i = 0; i < this.length; i += nth) {
-        result[i / nth] = this.substr(i, nth);
-    }
-
-    return result;
-}
-
-class message_url {
-    constructor(url = window.location.href) {
-        this.url = url;
-
-        let _subdirectories = new URL(window.location.href).pathname.split("/").filter(subdirectory => subdirectory.length > 0);
-
-        this.guildID = _subdirectories[1];
-        this.channelID = _subdirectories[2];
-    }
-}
-
-var discord = {
-    run: function(func) {
-        let wpRequire;
-        webpackChunkdiscord_app.push([
-            [Date.now()], // chunk ID. using the current time like this works fine
-            {}, // modules mapped `id: (wpModule, wpExports, wpRequire) => {}`. these chunks can be loaded by `wpRequire(id)`
-            _wpRequire => (wpRequire = _wpRequire), // callback that runs when this chunk is loaded (it will run immediately after calling push(). assigning wpRequre here to an outside variable makes it convenient to use
-        ]);
-        for (var i in wpRequire.c) {
-            let exports = wpRequire.c[i].exports;
-            if (typeof(exports?.[func]) == "function") {
-                return exports?.[func]();
-            } else if (wpRequire.c[i].id == func) {
-                return wpRequire.c[i].id;
+            get userID() { // readonly
+                return atob(this.token.split(".")[0]);
             }
-        }
-    },
 
-    User: class {
-        constructor(token = discord.run("getToken"), url_config = new message_url()) {
-            this.token = token;
-            this.id = atob(token.match("^[^.]+")[0]);
-            this.url_config = url_config;
+            constructor(token = Discord.get("getToken")()) {
+                [, this.guildID, this.channelID] = new URL(window.location.href).pathname.split("/").filter(Boolean);
+                this.token = token;
 
-            this.actions = {
-                block: async (id) => {
-                    fetch(`https://discord.com/api/v9/users/@me/relationships/${id}`, {
-                        "headers": {
-                            "authorization": this.token,
-                            "content-type": "application/json",
-                        },
-                        "body": "{\"type\":2}",
-                        "method": "PUT",
-                    })
+                this.messages.getLastUserMessage().then(messageID => {    
+                    this.messageID = messageID;
+                })
+            }
+
+            actions = {
+                block: userID => {
+                    this.#fetchApi("/users/@me/relationships/" + userID, "PUT", {
+                        "type": 2,
+                    });
                 },
 
-                unblock: async (id) => {
-                    fetch(`https://discord.com/api/v9/users/@me/relationships/${id}`, {
-                        "headers": {
-                            "authorization": this.token,
-                            "content-type": "application/json",
-                        },
-                        "method": "DELETE",
-                    });
+                unblock: userID => {
+                    this.#fetchApi("/users/@me/relationships/" + userID, "DELETE");
                 }
             }
 
-            this.message = {
-                get_last_message: async () => {
-                    let result = null;
-
-                    let fetched = await fetch(`https://discord.com/api/v9/channels/${this.url_config.channelID}/messages?limit=1`, {
-                        "headers": {
-                            "authorization": this.token,
-                        },
-                        "method": "GET",
-                    }).then(data => data.json()).then(response => {
-                        if (response.length > 0) {
-                            result = response[0].id;
-                        }
-                    });
-
-                    return result;
+            messages = {
+                getLastMessage: async () => {
+                    return this.#fetchApi(`/channels/${this.channelID}/messages?limit=1`)
+                        .then(data => data.json())
+                        .then(response => response?.[0].id);
                 },
 
-                get_last_user_message: async () => {
-                    let result = null;
+                getLastUserMessage: async () => {
+                    return (await this.#fetchApi(`/channels/${this.channelID}/messages?limit=100`)
+                        .then(data => data.json())
+                        .then(response => response.find(message => message?.author.id === this.userID)?.id)
+                    )
 
-                    let fetched = await fetch(`https://discord.com/api/v9/channels/${this.url_config.channelID}/messages?limit=100`, {
-                        "headers": {
-                            "authorization": this.token,
-                        },
-                        "method": "GET",
-                    }).then(data => data.json()).then(response => {
-                        response = response.filter(message => message.author.id == this.id);
-                        if (response.length > 0) {
-                            result = response[0].id;
-                        }
-                    })
+                        ??
 
-                    if (!result) {
-                        let fetched1 = await fetch(`https://discord.com/api/v9/channels/${this.url_config.channelID}/messages/search?author_id=${this.id}`, {
-                            "headers": {
-                                "authorization": this.token,
-                            },
-                            "method": "GET",
-                        }).then(data => data.json()).then(response => {
-                            if (response.messages.length > 0) {
-                                result = response.messages[0][0].id;
-                            }
-                        });
-                    }
-                    return result;
+                        this.#fetchApi(`/channels/${this.channelID}/messages/search?author_id=` + this.userID)
+                            .then(data => data.json())
+                            .then(response => response?.messages[0][0].id);
                 },
 
-                send: (text) => {
-                    let body = {
+                send: text => {
+                    this.#fetchApi(`/channels/${this.channelID}/messages`, "POST", {
                         "mobile_network_type": "unknown",
                         "content": text,
                         "nonce": Date.now(),
                         "tts": false,
                         "flags": 0
-                    };
-
-                    fetch(`https://discord.com/api/v9/channels/${this.url_config.channelID}/messages`, {
-                        "headers": {
-                            "authorization": this.token,
-                            "content-type": "application/json",
-                        },
-                        "body": JSON.stringify(body),
-                        "method": "POST",
                     });
                 },
 
-                delete: async (id) => {
-                    if (!id) {
-                        id = await this.message.get_last_user_message();
-                    }
-
-                    fetch(`https://discord.com/api/v9/channels/${this.url_config.channelID}/messages/${id}`, {
-                        "headers": {
-                            "authorization": this.token,
-                            "content-type": "application/json",
-                        },
-                        "method": "DELETE",
-                    });
+                delete: () => {
+                    this.#fetchApi(`/channels/${this.channelID}/messages/` + this.messageID, "DELETE")
                 },
 
-                edit: async (text, id) => {
-                    if (!id) {
-                        id = await this.message.get_last_user_message();
-                    }
-
-                    let body = {
+                edit: text => {
+                    this.#fetchApi(`/channels/${this.channelID}/messages/` + this.messageID, "PATCH", {
                         "mobile_network_type": "unknown",
                         "content": text,
                         "nonce": Date.now(),
                         "tts": false,
                         "flags": 0
-                    };
-
-                    fetch(`https://discord.com/api/v9/channels/${this.url_config.channelID}/messages/${id}`, {
-                        "headers": {
-                            "authorization": this.token,
-                            "content-type": "application/json",
-                        },
-                        "body": JSON.stringify(body),
-                        "method": "PATCH",
                     });
                 },
 
-                reply: async (text, id) => {
-                    if (!id) {
-                        id = await this.message.get_last_message();
-                    }
-
-                    let body = {
+                reply: text => {
+                    this.#fetchApi(`/channels/${this.channelID}/messages`, "POST", {
                         "mobile_network_type": "unknown",
                         "message_reference": {
-                            "message_id": id,
-                            "channel_id": url_config.channelID,
-                            "guild_id": ((url_config.guildID == "@me") ? null : url_config.guildID)
+                            "message_id": this.messageID,
+                            "channel_id": this.channelID,
+                            "guild_id": this.guildID !== "@me" && this.guildID // : null
                         },
                         "content": text,
                         "nonce": Date.now(),
                         "tts": false,
                         "flags": 0
-                    };
-
-                    fetch(`https://discord.com/api/v9/channels/${this.url_config.channelID}/messages`, {
-                        "headers": {
-                            "authorization": this.token,
-                            "content-type": "application/json",
-                        },
-                        "body": JSON.stringify(body),
-                        "method": "POST",
                     });
                 },
 
-                react: async (emoji, id) => {
-                    if (!id) {
-                        id = await this.message.get_last_message();
-                    }
-
-                    fetch(`https://discord.com/api/v9/channels/${this.url_config.channelID}/messages/${id}/reactions/${encodeURI(emoji)}/%40me?location=Message`, {
-                        "headers": {
-                            "authorization": this.token,
-                            "content-type": "application/json",
-                        },
-                        "method": "PUT",
-                    })
+                react: emoji => {
+                    this.#fetchApi(`/channels/${this.channelID}/messages/${this.messageID}/reactions/${encodeURIComponent(emoji)}/%40me?location=Message`, "PUT")
                 },
 
-                unreact: async (emoji, id) => {
-                    if (!id) {
-                        id = await this.message.get_last_message();
-                    }
-
-                    fetch(`https://discord.com/api/v9/channels/${this.url_config.channelID}/messages/${id}/reactions/${encodeURI(emoji)}/%40me?location=Message`, {
-                        "headers": {
-                            "authorization": this.token,
-                            "content-type": "application/json",
-                        },
-                        "method": "DELETE",
-                    })
+                unreact: emoji => {
+                    this.#fetchApi(`/channels/${this.channelID}/messages/${this.messageID}/reactions/${encodeURIComponent(emoji)}/%40me?location=Message`, "DELETE")
                 },
             };
         }
