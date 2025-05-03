13a18d117557363c733fff44c4d9cabdde670d3d
a <102840087+kingdudely@users.noreply.github.com>
2025-05-03 11:11:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -32,7 +32,7 @@
         },
 
         User: class {
-            #fetchApi(api, method, body) {
+            fetchApi(api, method, body) {
                 return fetch(apiUrl + api, {
                     "headers": {
                         "authorization": this.token,
@@ -41,55 +41,57 @@
                     body: JSON.stringify(body),
                     method
                 })
-            };
+            }
+
+            setIDsFromURL(url = window.location.href) {
+                [, this.guildID, this.channelID] = new URL(url).pathname.split("/").filter(Boolean);
+            }
 
             get userID() { // readonly
                 return atob(this.token.split(".")[0]);
             }
 
             constructor(token = Discord.get("getToken")()) {
-                [, this.guildID, this.channelID] = new URL(window.location.href).pathname.split("/").filter(Boolean);
+                this.setIDsFromURL();
                 this.token = token;
 
-                this.messages.getLastUserMessage().then(messageID => {    
-                    this.messageID = messageID;
-                })
+                this.messages.getLastUserMessage().then(messageID => this.messageID = messageID);
             }
 
             actions = {
                 block: userID => {
-                    this.#fetchApi("/users/@me/relationships/" + userID, "PUT", {
+                    this.fetchApi("/users/@me/relationships/" + userID, "PUT", {
                         "type": 2,
                     });
                 },
 
                 unblock: userID => {
-                    this.#fetchApi("/users/@me/relationships/" + userID, "DELETE");
+                    this.fetchApi("/users/@me/relationships/" + userID, "DELETE");
                 }
             }
 
             messages = {
                 getLastMessage: async () => {
-                    return this.#fetchApi(`/channels/${this.channelID}/messages?limit=1`)
+                    return this.fetchApi(`/channels/${this.channelID}/messages?limit=1`)
                         .then(data => data.json())
                         .then(response => response?.[0].id);
                 },
 
                 getLastUserMessage: async () => {
-                    return (await this.#fetchApi(`/channels/${this.channelID}/messages?limit=100`)
+                    return (await this.fetchApi(`/channels/${this.channelID}/messages?limit=100`)
                         .then(data => data.json())
                         .then(response => response.find(message => message?.author.id === this.userID)?.id)
                     )
 
                         ??
 
-                        this.#fetchApi(`/channels/${this.channelID}/messages/search?author_id=` + this.userID)
+                        this.fetchApi(`/channels/${this.channelID}/messages/search?author_id=` + this.userID)
                             .then(data => data.json())
                             .then(response => response?.messages[0][0].id);
                 },
 
                 send: text => {
-                    this.#fetchApi(`/channels/${this.channelID}/messages`, "POST", {
+                    this.fetchApi(`/channels/${this.channelID}/messages`, "POST", {
                         "mobile_network_type": "unknown",
                         "content": text,
                         "nonce": Date.now(),
@@ -99,11 +101,11 @@
                 },
 
                 delete: () => {
-                    this.#fetchApi(`/channels/${this.channelID}/messages/` + this.messageID, "DELETE")
+                    this.fetchApi(`/channels/${this.channelID}/messages/` + this.messageID, "DELETE")
                 },
 
                 edit: text => {
-                    this.#fetchApi(`/channels/${this.channelID}/messages/` + this.messageID, "PATCH", {
+                    this.fetchApi(`/channels/${this.channelID}/messages/` + this.messageID, "PATCH", {
                         "mobile_network_type": "unknown",
                         "content": text,
                         "nonce": Date.now(),
@@ -113,7 +115,7 @@
                 },
 
                 reply: text => {
-                    this.#fetchApi(`/channels/${this.channelID}/messages`, "POST", {
+                    this.fetchApi(`/channels/${this.channelID}/messages`, "POST", {
                         "mobile_network_type": "unknown",
                         "message_reference": {
                             "message_id": this.messageID,
@@ -128,11 +130,11 @@
                 },
 
                 react: emoji => {
-                    this.#fetchApi(`/channels/${this.channelID}/messages/${this.messageID}/reactions/${encodeURIComponent(emoji)}/%40me?location=Message`, "PUT")
+                    this.fetchApi(`/channels/${this.channelID}/messages/${this.messageID}/reactions/${encodeURIComponent(emoji)}/%40me?location=Message`, "PUT")
                 },
 
                 unreact: emoji => {
-                    this.#fetchApi(`/channels/${this.channelID}/messages/${this.messageID}/reactions/${encodeURIComponent(emoji)}/%40me?location=Message`, "DELETE")
+                    this.fetchApi(`/channels/${this.channelID}/messages/${this.messageID}/reactions/${encodeURIComponent(emoji)}/%40me?location=Message`, "DELETE")
                 },
             };
         }
